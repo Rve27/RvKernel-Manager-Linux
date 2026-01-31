c0e38ec537d8c21bbbce3d76e5ef4fad8f42a36d
Rve27 <rve27github@gmail.com>
2026-02-01 06:13:01 +0700

    data: HomeItem: Add option to change container icon shape
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- composeApp/src/jvmMain/kotlin/com/rve/rvkernelmanager/ui/data/HomeItem.kt
+++ composeApp/src/jvmMain/kotlin/com/rve/rvkernelmanager/ui/data/HomeItem.kt
@@ -1,10 +1,11 @@
 package com.rve.rvkernelmanager.ui.data
 
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.graphics.Shape
 
 data class HomeItem(
     val icon: AppIcon,
+    val containerIconShape: Shape = CircleShape,
     val title: String,
     val summary: String,
 )
