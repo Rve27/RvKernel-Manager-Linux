aadfa2bc15265c716944921d1b47f11941a48399
Rve27 <rve27github@gmail.com>
2026-01-31 07:46:02 +0700

    data: Move AppIcon to its own file
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- composeApp/src/jvmMain/kotlin/com/rve/rvkernelmanager/ui/data/HomeItem.kt
+++ composeApp/src/jvmMain/kotlin/com/rve/rvkernelmanager/ui/data/HomeItem.kt
@@ -3,11 +3,6 @@ package com.rve.rvkernelmanager.ui.data
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 
-
-sealed interface AppIcon {
-    data class PainterIcon(val painter: Painter) : AppIcon
-    data class ImageVectorIcon(val imageVector: ImageVector) : AppIcon
-}
 data class HomeItem(
     val icon: AppIcon,
     val title: String,
